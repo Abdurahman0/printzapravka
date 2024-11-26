3ac32d6aa9605df1b25c4418ed5d9d58ba712b8a
Dumbie <wasleyron668@gmail.com>
2024-11-26 07:21:05 +0500

    added telegram bot

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,56 @@
+import { useState } from 'react'
+import axios from 'axios'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 function Form() {
+	const [formData, setFormData] = useState({
+		firstName: '',
+		lastName: '',
+		phone: '',
+	})
+	const [IsLoading, setIsLoading] = useState(false)
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target
+		setFormData({ ...formData, [name]: value })
+	}
+
+	const SendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		setIsLoading(true)
+		const token = import.meta.env.VITE_API_KEY
+		const chat_id = import.meta.env.VITE_CHAT_ID
+		const url = `https://api.telegram.org/bot${token}/sendMessage`
+		const messageContent = `Ismi: ${formData.firstName} \n Familyasi: ${formData.lastName} \n Phone: ${formData.phone}`
+
+		axios
+			.post(url, {
+				chat_id: chat_id,
+				text: messageContent,
+			})
+			.then(() => {
+				setFormData({ firstName: '', lastName: '', phone: '' })
+				toast.success('Muvaffaqiyatli yuborildi')
+				setIsLoading(false)
+			})
+			.catch(err => {
+				toast.error(`Yuborishda xatolik: ${err.message}`)
+				setIsLoading(false)
+			})
+	}
 	return (
 		<div className='relative min-h-screen flex items-center justify-center bg-gradient-to-r from-[#1B2838] to-[#2A3F54] p-6'>
+			<ToastContainer />
 			<div className='max-w-4xl w-full bg-gray-800 bg-opacity-80 shadow-lg rounded-xl p-8 md:p-12 transform transition duration-700 hover:scale-105 border border-gray-700'>
 				<h2 className='text-4xl font-semibold text-center text-white mb-10 tracking-wider'>
 					Связаться с нами
 				</h2>
-				<form className='grid md:grid-cols-2 gap-8'>
+				<form
+					className='grid md:grid-cols-2 gap-8'
+					id='myForm'
+					onSubmit={SendMessage}
+				>
 					<div className='left flex flex-col space-y-6'>
 						<div className='relative'>
 							<input
@@ -13,6 +58,8 @@ function Form() {
 								name='firstName'
 								placeholder='Имя'
 								required
+								value={formData.firstName}
+								onChange={handleInputChange}
 								className='w-full px-4 py-3 bg-transparent border border-gray-600 rounded-lg focus:outline-none focus:border-blue-400 text-white placeholder-gray-400 transition duration-300'
 							/>
 						</div>
@@ -22,6 +69,8 @@ function Form() {
 								name='lastName'
 								placeholder='Фамилия '
 								required
+								value={formData.lastName}
+								onChange={handleInputChange}
 								className='w-full px-4 py-3 bg-transparent border border-gray-600 rounded-lg focus:outline-none focus:border-blue-400 text-white placeholder-gray-400 transition duration-300'
 							/>
 						</div>
@@ -32,14 +81,21 @@ function Form() {
 								placeholder='940511494'
 								maxLength={9}
 								required
+								value={formData.phone}
+								onChange={handleInputChange}
 								className='w-full px-4 py-3 bg-transparent border border-gray-600 rounded-lg focus:outline-none focus:border-blue-400 text-white placeholder-gray-400 transition duration-300'
 							/>
 						</div>
 						<button
 							type='submit'
-							className='bg-indigo-500 text-[12px] sm:text-[16px] bg-opacity-20 text-indigo-300 font-semibold py-3 px-8 rounded-lg shadow-lg hover:bg-opacity-40 transition duration-300 transform hover:scale-105'
+							disabled={IsLoading}
+							className={`bg-indigo-500 text-[12px] sm:text-[16px] bg-opacity-20 text-indigo-300 font-semibold py-3 px-8 rounded-lg shadow-lg transition duration-300 transform ${
+								IsLoading
+									? 'cursor-not-allowed opacity-50'
+									: 'hover:bg-opacity-40 hover:scale-105'
+							}`}
 						>
-							Отправить
+							{IsLoading ? 'Загрузка...' : 'Отправить'}
 						</button>
 					</div>
 					<div className='right bg-gray-700 bg-opacity-60 rounded-lg shadow-lg flex items-center justify-center'>
